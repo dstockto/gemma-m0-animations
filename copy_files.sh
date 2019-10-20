76bc1e32f6c4b89ffb13b603ad475d57019d88c8
kellechu <github@kellechu.com>
2019-10-20 09:34:21 -0600

    Update copy_files.sh help instructions

--- copy_files.sh
+++ copy_files.sh
@@ -7,9 +7,12 @@ function usage() {
   echo "$0 CIRCUITPY"
   echo
   echo "Argument should be the name of your Gemma drive."
+  echo 'Check to make sure Gemma is plugged in to USB and turned on.'
+  echo
 }
 
 if [ $# -eq 0 ]; then
+  echo
   echo "Need name of Gemma"
   echo
   usage
