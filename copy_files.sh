2b8e2e4256ba79135096d6160592a7768ae5ff89
David Stockton <dave@davidstockton.com>
2019-10-19 21:38:18 -0600

    Add script to copy files to the Gemma

---
+++ copy_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function usage() {
+    echo "Usage:"
+    echo "------------------------"
+    echo
+    echo "$0 CIRCUITPY"
+    echo
+    echo "Argument should be the name of your Gemma drive."
+}
+
+if [ $# -eq 0 ]; then
+  echo "Need name of Gemma"
+  echo
+  usage
+  exit
+fi
+
+cp main.py /Volumes/$1/main.py
+cp -R lib/ /Volumes/$1/lib
